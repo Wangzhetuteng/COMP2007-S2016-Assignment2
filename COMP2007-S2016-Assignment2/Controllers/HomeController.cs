d0dd7963716d86d26564207c8f80556890954b68
Wangzhetuteng <ericdicaprio@outlook.com>
2016-07-26 18:30:57 -0400

    updated project files

--- COMP2007-S2016-Assignment2/Controllers/HomeController.cs
+++ COMP2007-S2016-Assignment2/Controllers/HomeController.cs
@@ -8,22 +8,10 @@ namespace COMP2007_S2016_Assignment2.Controllers
 {
     public class HomeController : Controller
     {
+        //
+        // GET: /HOME/
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
---
+++ COMP2007-S2016-Assignment2/Models/Genre.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using COMP2007_S2016_Assignment2.Models;
+
+namespace COMP2007_S2016_Assignment2.Models
+{
+    public class Genre
+    {
+        /// <summary>
+        /// This is the empty constructor
+        /// </summary>
+        public Genre()
+        {
+
+        }
+        /// <summary>
+        /// This constructor takes one parameter = Name
+        /// </summary>
+        /// <param name="Name"></param>
+        public Genre(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public int GenreID { get; set; }
+        public string Name { get; set; }
+    }
+}
