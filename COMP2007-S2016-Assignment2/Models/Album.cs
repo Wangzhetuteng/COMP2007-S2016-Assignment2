a8bbe1e75b39e1181b79afb8abeb3fba28fd1490
Wangzhetuteng <ericdicaprio@outlook.com>
2016-07-28 10:43:17 -0400

    added Data Annotations for some Validation and Display Enhancements - Closes Issue #13

--- COMP2007-S2016-Assignment2/Models/Album.cs
+++ COMP2007-S2016-Assignment2/Models/Album.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace COMP2007_S2016_Assignment2.Models
 {
-
+    [Bind(Exclude = "AlbumId")]
     public class Album
     {
         /// <summary>
@@ -24,11 +27,18 @@ namespace COMP2007_S2016_Assignment2.Models
             this.Title = Title;
         }
 
+        [ScaffoldColumn(false)]
         public virtual int AlbumId { get; set; }
+        [DisplayName("Genre")]
         public virtual int GenreId { get; set; }
+        [DisplayName("Artist")]
         public virtual int ArtistId { get; set; }
+
         public virtual string Title { get; set; }
+
         public virtual decimal Price { get; set; }
+        [DisplayName("Album Art URL")]
+
         public virtual string AlbumArtUrl { get; set; }
         public virtual Genre Genre { get; set; }
         public virtual Artist Artist { get; set; }
