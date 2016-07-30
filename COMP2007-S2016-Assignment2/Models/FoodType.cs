26e866b1e9908f4f781ce22f6101e8fd2ba882ec
Wangzhetuteng <ericdicaprio@outlook.com>
2016-07-30 18:51:51 -0400

    updated FoodType and SubMenu - Model Classes

---
+++ COMP2007-S2016-Assignment2/Models/FoodType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using COMP2007_S2016_Assignment2.Models;
+
+namespace COMP2007_S2016_Assignment2.Models
+{
+    public class FoodType
+    {
+        /// <summary>
+        /// This is the empty constructor
+        /// </summary>
+        public FoodType()
+        {
+
+        }
+        /// <summary>
+        /// This constructor takes one parameter = Name
+        /// </summary>
+        /// <param name="Name"></param>
+        public FoodType(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public int TypeId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<SubMenu> SubMenus { get; set; }
+    }
+}
---
+++ COMP2007-S2016-Assignment2/Models/SubMenu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COMP2007_S2016_Assignment2.Models
+{
+    [Bind(Exclude = "SubMenuId")]
+    public class SubMenu
+    {
+        /// <summary>
+        /// This is the empty constructor
+        /// </summary>
+        public SubMenu()
+        {
+
+        }
+        /// <summary>
+        /// This constructor takes one parameter - Title
+        /// </summary>
+        /// <param name="Title"></param>
+        public SubMenu(string Title)
+	    {
+            this.Title = Title;
+        }
+
+        [ScaffoldColumn(false)]
+        public virtual int SubMenuId { get; set; }
+        [DisplayName("Type")]
+        public virtual int TypeId { get; set; }
+
+
+        public virtual string Title { get; set; }
+
+        public virtual decimal Price { get; set; }
+        [DisplayName("SubMenu URL")]
+
+        public virtual string SubMenuUrl { get; set; }
+        public virtual FoodType Type { get; set; }
+
+    }
+}
